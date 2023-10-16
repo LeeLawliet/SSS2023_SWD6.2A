7acd52a91c5fd7cb48e76fbe3d77886c48f1c861
DESKTOP-5V3R713\User <lee.xerri.e21932@mcast.edu.mt>
2023-10-16 15:01:15 +0200

    SumEvenNumbers

--- summation.php
+++ summation.php
@@ -1,2 +1,17 @@
 <?php
+
+function SumEvenNumbers($arr)
+{
+    $sum = 0;
+
+    foreach ($arr as $num)
+    {
+        $num % 2 == 0 ? $sum += $num : $sum;
+    }
+
+    echo $sum;
+}
+
+$numbers = [2, 4, 6, 3, 8, 7];
+SumEvenNumbers($numbers);
 ?>
