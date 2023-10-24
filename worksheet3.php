646689ad20226f56104eeba39fce73e37d8b848d
DESKTOP-5V3R713\User <lee.xerri.e21932@mcast.edu.mt>
2023-10-24 13:32:21 +0200

    Finish worksheet 3

--- worksheet3.php
+++ worksheet3.php
@@ -3,22 +3,32 @@
     {
         public $model;
         public $color;
+        public static $wheels = 4;
 
-        function _construct($pModel, $pColor)
+        protected $price;
+
+        function __construct($model, $color, $price)
         {
-            $this->model = $pModel;
-            $this->color = $pColor;
+            $this->model = $model;
+            $this->color = $color;
+            $this->price = $price;
         }
 
-        function set_model($pModel)
-        {
-            $this->model = $pModel;
-        }
+        // Q1
+        // function set_model($pModel)
+        // {
+        //     $this->model = $pModel;
+        // }
 
-        function set_color($pColor)
-        {
-            $this->color = $pColor;
-        }
+        // function set_color($pColor)
+        // {
+        //     $this->color = $pColor;
+        // }
+
+        // function set_price($pPrice)
+        // {
+        //     $this->price = $pPrice;
+        // }
 
         function get_model()
         {
@@ -29,27 +39,80 @@
         {
             return $this->color;
         }
+
+        function get_price()
+        {
+            return $this->price;
+        }
+
+        static function get_wheels()
+        {
+            return self::$wheels;
+        }
     }
 
-    function main()
+    Class ElectricCar Extends Car
     {
-        // Q2
-        $ferrari = new Car("ferrari", "red");
-        $lambo = new Car("lamborghini", "yellow");
+        protected $batteryCapacity;
 
-        // Q1
-        // $ferrari->set_model("ferrari");
-        // $ferrari->set_color("red");
+        public static function Question()
+        {
+            echo "Am I a real car?";
+        }
 
-        // $lambo = new Car();
-        // $lambo->set_model("lamborghini");
-        // $lambo->set_color("yellow");
+        function __construct($model, $color, $price, $batteryCapacity)
+        {
+            parent::__construct($model, $color, $price);
+            $this->batteryCapacity = $batteryCapacity;
+        }
 
-        echo $ferrari->get_model();
-        echo $ferrari->get_color();
-        echo $lambo->get_model();
-        echo $lambo->get_color();
+        function get_batterycapacity()
+        {
+            return $this->batteryCapacity;
+        }
     }
 
-    main();
+    //  Q1
+    // $ferrari = new Car("ferrari", "red", 20000);
+    // $lambo = new Car("lamborghini", "yellow", 30000);
+
+    // $ferrari->set_model("ferrari");
+    // $ferrari->set_color("red");
+
+    // $lambo = new Car();
+    // $lambo->set_model("lamborghini");
+    // $lambo->set_color("yellow");
+
+    // echo $ferrari->get_model();
+    // echo $ferrari->get_color();
+    // echo $lambo->get_model();
+    // echo $lambo->get_color();
+
+            
+    // Q2
+    // $Porsche = new Car("Porsche", "Black", 90000);
+
+    // echo $Porsche->get_model();
+    // echo "\n";
+    // echo $Porsche->get_color();
+    // echo "\n";
+    // echo $Porsche->get_price();
+    // echo "\n";
+    // echo $Porsche::get_wheels();
+
+    // Q3
+    $Tesla = new ElectricCar("Tesla", "White", 120000, "50kWh");
+
+    echo $Tesla->get_model();
+    echo "\n";
+    echo $Tesla->get_color();
+    echo "\n";
+    echo $Tesla->get_price();
+    echo "\n";
+    echo $Tesla::get_wheels();
+    echo "\n";
+    echo $Tesla->get_batterycapacity();
+    echo "\n";
+    $Tesla::Question();
+    echo "\n";
 ?>
