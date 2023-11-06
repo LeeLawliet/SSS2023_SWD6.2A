ff183c6638cc5801e338059b4905076571e2aeb9
LeeLawliet <lee.xerri.e21932@mcast.edu.mt>
2023-11-06 14:12:05 +0000

    set up contacts and companies model and add data

--- database/migrations/2023_11_06_134127_create_contacts.php
+++ database/migrations/2023_11_06_134127_create_contacts.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('phone')->nullable;
+            $table->string('phone')->nullable();
             $table->string('email');
             $table->string('address');
             $table->foreignid('company_id')->constrained()->onDelete('cascade');
