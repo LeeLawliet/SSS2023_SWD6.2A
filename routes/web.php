19d25601385e7b6dcb063575636a989fcdd7ef73
LeeLawliet <lee.xerri.e21932@mcast.edu.mt>
2023-11-13 13:37:53 +0000

    create name routes

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,17 @@ Route::get('/', function () {
 Route::get('/contacts', function()
 {
     return '<h1>All contacts</h1>';
-});
+})->name('contacts.index');
 
 Route::get('/contacts/create', function()
 {
     return '<h1>Add new contact</h1>';
-});
+})->name('contacts.create');
 
 // /contacts/2
 // returns all details of 2
 Route::get('/contacts/{id}', function($id)
 {
     return App\Models\Contact::find($id);
-});
+})->name('contacts.show');
 
