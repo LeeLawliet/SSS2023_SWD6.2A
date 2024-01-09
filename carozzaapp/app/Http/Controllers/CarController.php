a2851082c32667dc4d991efacbdfa891c664316c
LeeLawliet <lee.xerri.e21932@mcast.edu.mt>
2024-01-09 13:09:41 +0000

    Filter car by manufacturer works

--- carozzaapp/app/Http/Controllers/CarController.php
+++ carozzaapp/app/Http/Controllers/CarController.php
@@ -21,7 +21,7 @@ class CarController extends Controller
     }
 
     function create() {
-        $manufacturers = Manufacturer::all(); // passing manufacturers to be iterated in drop-down list
+        $manufacturers = Manufacturer::orderBy('name')->pluck('name','id')->prepend('All Manufacturers', '');
         return view('cars.create', compact('manufacturers'));
     }
 
